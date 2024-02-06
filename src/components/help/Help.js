ffe6d95b4e8489fe44cf171137ac9e91563f0916
kavya singampalli <singampallikavya01@gmail.com>
2024-02-06 12:00:01 +0530

    created footer section

--- src/components/help/Help.js
+++ src/components/help/Help.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Subscribe from "./subscribe/Subscribe";
 import "./Help.css";
+import HelpFooter from "./helpfooter/Helpfooter";
 
 const Help = () => {
   return (
-    <div className="help-style">
-      <Subscribe />
-    </div>
+    (
+      <div className="help-style">
+        <Subscribe />
+      </div>
+    ),
+    (
+      <div>
+        <HelpFooter />
+      </div>
+    )
   );
 };
 
---
+++ src/components/help/helpfooter/Helpfooter.js
@@ -0,0 +1,99 @@
+import React from "react";
+import "./Helpfooter.css";
+import icon from "./icon.svg";
+import fb from "./fblogo.svg";
+import twitter from "./twiterlogo.svg";
+import insta from "./instalogo.svg";
+
+const HelpFooter = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ marginRight: "200px" }}>
+        <div className="footer-logo">
+          <a href="#">
+            <img src={icon}></img>
+          </a>
+        </div>
+        <p className="helpfooter-p">
+          LaslesVPN is a private virtual network that has unique features and
+          has high security.
+        </p>
+
+        <div className="social-icon">
+          <a href="https://www.facebook.com/">
+            <img src={fb}></img>
+          </a>
+          <a href="https://Twitter.com/">
+            <img src={twitter}></img>
+          </a>
+          <a href="https://www.Instagram.com/">
+            <img src={insta}></img>
+          </a>
+          <p>© 2020 LaslesVPN</p>
+        </div>
+      </div>
+
+      <div>
+        <h4>Products</h4>
+        <ul>
+          <li>
+            <a href="#">Download</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Locations</a>
+          </li>
+          <li>
+            <a href="#">Server</a>
+          </li>
+          <li>
+            <a href="#">Countries</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h4>Engage</h4>
+        <ul>
+          <li>
+            <a href="#">LaslesVPN</a>
+          </li>
+          <li>
+            <a href="#">FAQ</a>
+          </li>
+          <li>
+            <a href="#">Tutorials</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#">Terms of Service</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h4>Earn Money</h4>
+        <ul>
+          <li>
+            <a href="#">Affiliate</a>
+          </li>
+          <li>
+            <a href="#">Become Partner</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default HelpFooter;
